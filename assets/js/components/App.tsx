8a149cd90a5c7bb87f4efb81f0e989a4de94510c
Mathieu Lajugie <mathieu@caring.com>
2019-05-31 11:47:11 -0700

    setup Apollo client

--- assets/js/components/App.tsx
+++ assets/js/components/App.tsx
@@ -1,6 +1,19 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import { ApolloProvider } from "react-apollo"
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { HttpLink } from "apollo-link-http"
 
 import Root from "./Root"
 
-ReactDOM.render(<Root />, document.getElementById("react-app"))
+const cache = new InMemoryCache()
+const link = new HttpLink({ uri: "http://localhost:4000/api" })
+const client = new ApolloClient({ cache, link })
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Root />
+  </ApolloProvider>,
+  document.getElementById("react-app")
+)
--- assets/js/components/Root.tsx
+++ assets/js/components/Root.tsx
@@ -1,6 +1,17 @@
 import React, { Component, ChangeEvent } from "react"
 import * as ReactDOM from "react-dom"
 import { DatePicker, message, Alert } from "antd"
+import { Query } from "react-apollo"
+import gql from "graphql-tag"
+
+const QUERY = gql`
+  query {
+    helloWorld {
+      id
+      world
+    }
+  }
+`
 
 export default class extends Component {
   state = { date: null }
@@ -18,6 +29,13 @@ export default class extends Component {
         <DatePicker onChange={this.handleChange} />
         <div style={{ marginTop: 20 }}>
           <Alert message={`Selected date: ${date ? date.format("YYYY-MM-DD") : "None"}`} type="success" />
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <Query query={QUERY}>
+            {({ data, loading, _ }) =>
+              loading || <Alert message={`data: ${JSON.stringify(data, null, 4)}`} type="info" />
+            }
+          </Query>
         </div>
       </div>
     )
